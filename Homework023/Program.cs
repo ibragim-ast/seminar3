c0883e47d3355956938e1cf65206ef9c6aff54f2
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-12 20:09:13 +0300

    add Homework023

---
+++ Homework023/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int Cube(int num)
+{
+    while (index < num)
+    num1 = num * num * num;
+    index++;
+    return num1;
+}
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Cube(a);
+Console.WriteLine(b);
+
+
