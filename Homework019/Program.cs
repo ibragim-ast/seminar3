d5d77c7d38016765364b64508a47f9b5e7355d14
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-13 18:00:59 +0300

    add solution Homework019

---
+++ Homework019/Program.cs
@@ -0,0 +1,18 @@
+﻿int Palindrome(int num1)
+{
+    int num2 = 0;
+    int digit = 0;
+    while (num1 > 0)
+    {          
+        digit = num1 % 10; 
+        num1 = num1 / 10;
+        num2 = num2 * 10 + digit;           
+    }
+    return num2;
+}
+int result = Palindrome(45654);
+Console.WriteLine(result);
+
+
+
+
