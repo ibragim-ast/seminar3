f497f953696719188480a6f03a740086583d7b5e
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-13 17:59:13 +0300

    solution Homework019

--- Homework023/Program.cs
+++ Homework023/Program.cs
@@ -1,4 +1,5 @@
-﻿void TableCube(int num)
+﻿//Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+void TableCube(int num)
 {
     int i = 1;
     while (num >= i)
