b98877be7008bd66a6ac629e3eb6485cc6830ea3
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-13 18:22:03 +0300

    add solution Homework021

---
+++ Homework021/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на взод координаты двух точку и находит расстояние между ними в 
+//3D пространстве
+
+void FindDistance(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double num1 = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));  
+    Console.WriteLine(num1);
+}
+
+FindDistance(7, -5, 0, 1, -1, -9);
