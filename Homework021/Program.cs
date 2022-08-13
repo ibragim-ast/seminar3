56f8ccc455f90f4eb06f8e1c86cc6de68c6c9084
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-13 18:59:24 +0300

    update solution Homework021

--- Homework021/Program.cs
+++ Homework021/Program.cs
@@ -1,10 +1,23 @@
 ﻿//Напишите программу, которая принимает на взод координаты двух точку и находит расстояние между ними в 
 //3D пространстве
 
-void FindDistance(double xa, double ya, double za, double xb, double yb, double zb)
+double FindDistance3D(double Xa, double Ya, double Za, double Xb, double Yb, double Zb)
 {
-    double num1 = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));  
-    Console.WriteLine(num1);
+    double num1 = Math.Sqrt((Xb - Xa) * (Xb - Xa) + (Yb - Ya) * (Yb - Ya) + (Zb - Za) * (Zb - Za));  
+    return num1;
 }
 
-FindDistance(7, -5, 0, 1, -1, -9);
+Console.WriteLine("Введите координаты Xa");
+double Xa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Ya");
+double Ya = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Za");
+double Za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Xb");
+double Xb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Yb");
+double Yb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Zb");
+double Zb = Convert.ToDouble(Console.ReadLine());
+double result = FindDistance3D(Xa, Ya, Za, Xb, Yb, Zb);
+Console.WriteLine($"А я тебя нашел {result}");
